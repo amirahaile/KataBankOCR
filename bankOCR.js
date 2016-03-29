9ea9d57144f91c2221a413e390c1568eb2c4a09f
amirahaile <amira.dhaile@gmail.com>
2016-03-28 17:58:11 -0700

    Handles file uploads.
    * Can upload multiple files; currently handles for one (first) file.
    * Returns if the file isn't a text file.
    * Uses FileReader to read content.

--- bankOCR.js
+++ bankOCR.js
@@ -1,3 +1,24 @@
 // Solution for KataBankOCR
 // Copyright (c) 2016 Amira Hailemariam
 // Coding challenge for TestDouble
+
+function readFile(event) {
+
+  var files = event.target.files; // FileList object
+  var file = files[0];
+
+  if (!file.type.match('text.*')) {
+    return;
+  }
+
+  var reader = new FileReader();
+
+  // Load event is fired once the content from
+  // the file is available.
+  reader.onload = function(event){
+    var fileContent = event.target.result;
+    console.log(fileContent);
+  };
+
+  reader.readAsText(file);
+}
