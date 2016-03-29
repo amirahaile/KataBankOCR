62e0f7d12c3fa725993ac0efa824defd6ce891e5
amirahaile <amira.dhaile@gmail.com>
2016-03-28 19:24:30 -0700

    Wrote formatEntries(content)
    * Takes content, a string produced by the FileReader.
    * Formats the entries into sub-arrays, e.g. [[[e1l1],[e1l2],[e1l3]],[[e2l1]…].
    * Removes the blank line between the entries.

--- bankOCR.js
+++ bankOCR.js
@@ -4,7 +4,7 @@
 
 function readFile(event) {
 
-  var files = event.target.files; // FileList object
+  var files = event.target.files; // FileList objects
   var file = files[0];
 
   if (!file.type.match('text.*')) {
@@ -16,9 +16,40 @@ function readFile(event) {
   // Load event is fired once the content from
   // the file is available.
   reader.onload = function(event){
-    var fileContent = event.target.result;
-    console.log(fileContent);
+    var fileContent = event.target.result; // String object
+    var entries = formatEntries(fileContent);
+    console.log(entries);
   };
 
   reader.readAsText(file);
+}
+
+function formatEntries(content) {
+  // i.e. [["entry1line1"], [entry1line2], [entry1line3], [""], [entry2line1], …]
+  var lines = content.split('\n');
+
+  // i.e. [[["line1"], ["line2"], ["line3"]], […], …]
+  var entries = [];
+  var entry = [];
+  for (i = 0; i < lines.length; i++) {
+    var linesInOneEntry = 3;
+
+    if (entry.length == linesInOneEntry) {
+      entries.push(entry);
+      entry = [];
+    }
+
+    // Don't include the blank lines.
+    if (lines[i].length !== 0) {
+      entry.push(lines[i]);
+    }
+  }
+
+  return entries;
+}
+
+function renderNumbers(entries) {
+  var accountNumbers = [];
+
+  
 }
